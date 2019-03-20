dca653adac7c83e8c5ced959fbc03da33991c03a
cbartram <cbartram3@gmail.com>
2019-03-20 08:04:47 -0400

    Added Boilerplate code

---
+++ index.js
@@ -0,0 +1,15 @@
+
+/**
+ * Creates a new promise which sometimes resolves and sometimes rejects but
+ * always takes 5 seconds for a response.
+ */
+let rnd = Math.floor(Math.random() * 6) + 1;
+const asyncAction = () => {
+
+}
+// Async we fire off 20 promises in literally a second
+console.log('Starting promises');
+for(let i = 0; i < 20; i++) {
+
+}
+console.log('All the promises (HTTP Requests) have been fired off in parallel');
